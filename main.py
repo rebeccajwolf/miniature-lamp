a3ddfae0812d7809fb3e2af1378b34dbdd5c5a8a
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2024-07-18 23:18:40 +0530

    Update

--- main.py
+++ main.py
@@ -372,7 +372,7 @@ def browserSetupv3(isMobile: bool = False, proxy: str = None) -> WebDriver:
 
     options.headless = True if ARGS.headless and ARGS.account_browser is None else False
     options.add_argument("--log-level=3")
-    options.add_argument("--incognito")
+    # options.add_argument("--incognito")
     options.add_argument("--blink-settings=imagesEnabled=false")
     options.add_argument("--ignore-certificate-errors")
     options.add_argument("--ignore-certificate-errors-spki-list")
