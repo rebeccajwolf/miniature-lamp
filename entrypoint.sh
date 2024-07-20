6ee045526dc214388909f2339dc0637dda8331d4
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2024-07-21 02:52:22 +0530

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sh ./clean_mem.sh ;
 exec gunicorn keep_alive:app --bind 0.0.0.0:8080 &
-python3 main.py --session --privacy --error --dont-check-for-updates --currency INR --dont-check-internet --skip-shopping --virtual-display --telegram --everyday;
+python3 main.py --privacy --error --dont-check-for-updates --currency INR --dont-check-internet --skip-shopping --virtual-display --telegram --everyday;
